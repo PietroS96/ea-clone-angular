2313aabefc9a948f3cd91ea8a39fb860ef076aa5
Pietro Scaduto <pietro.scaduto25@gmail.com>
2022-01-28 16:13:29 +0100

    feat: add folder footer and folder asset-footer

--- ea-clone/src/app/app.module.ts
+++ ea-clone/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterContainerComponent } from './component/footerComponent/footer-container/footer-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterContainerComponent
   ],
   imports: [
     BrowserModule
